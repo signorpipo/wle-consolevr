07ae928276d6b9fcc643b4d9bf4afc6e6f80afb3
Elia Ducceschi <eliaducceschi@hotmail.it>
2021-03-16 19:49:52 +0100

    Added same message counter

--- wle_consolevr/js/console-vr/console_vr_impl.js
+++ wle_consolevr/js/console-vr/console_vr_impl.js
@@ -12,13 +12,18 @@ PP.ConsoleVR = class ConsoleVR {
         this._myMaxLines = 25;
         this._myMaxMessages = 100;
         this._myMaxMessagesDeletePad = 20; // to prevent deleting at every message, delay the delete after the limit is exceed by this value
+
+        this._myOldConsole = [];
     }
 
     start() {
         window.addEventListener('keydown', this.press.bind(this));
 
-        this._myOldConsoleError = console.error;
+        this._myOldConsole[PP.ConsoleVR.MessageType.ERROR] = console.error;
         console.error = this._consolePrint.bind(this, PP.ConsoleVR.MessageType.ERROR);
+
+        this._myOldConsole[PP.ConsoleVR.MessageType.LOG] = console.log;
+        console.log = this._consolePrint.bind(this, PP.ConsoleVR.MessageType.LOG);
     }
 
     press() {
@@ -69,7 +74,7 @@ PP.ConsoleVR = class ConsoleVR {
 
     _consolePrint(messageType, ...args) {
         let message = this._argsToMessage(messageType, ...args);
-        this._myMessages.push(message);
+        this._addMessage(message);
 
         if (this._myMessages.length >= this._myMaxMessages + this._myMaxMessagesDeletePad) {
             this._myMessages = this._myMessages.slice(this._myMessages.length - this._myMaxMessages);
@@ -79,7 +84,7 @@ PP.ConsoleVR = class ConsoleVR {
             this._updateText();
         }
 
-        this._myOldConsoleError.apply(console, args);
+        this._myOldConsole[messageType].apply(console, args);
     }
 
     _argsToMessage(messageType, ...args) {
@@ -109,6 +114,19 @@ PP.ConsoleVR = class ConsoleVR {
 
         return formattedString;
     }
+
+    _addMessage(message) {
+        if (this._myMessages.length > 0) {
+            let lastMessage = this._myMessages[this._myMessages.length - 1];
+            if (lastMessage.hasSameInfo(message)) {
+                lastMessage.increaseCount();
+            } else {
+                this._myMessages.push(message);
+            }
+        } else {
+            this._myMessages.push(message);
+        }
+    }
 };
 
 PP.ConsoleVR.MessageType = {
@@ -121,10 +139,26 @@ PP.ConsoleVR.MessageType = {
 PP.ConsoleVR.Message = class Message {
     constructor(messageType, messageLines) {
         this.myType = messageType;
-        this.myLines = messageLines;
 
-        this.myText = messageLines.join("\n");
+        this._myOriginalText = messageLines.join("\n");
+        this._myOriginalLines = messageLines;
+        this._myMessagesCount = 1;
 
-        this.myMessagesCount = 1;
+        this.myText = this._myOriginalText.slice(0);
+        this.myLines = this._myOriginalLines.slice(0);
+    }
+
+    hasSameInfo(message) {
+        return this._myOriginalText == message._myOriginalText && this.myType == message.myType;
+    }
+
+    increaseCount() {
+        this._myMessagesCount += 1;
+
+        let countString = (("(x").concat(this._myMessagesCount)).concat(") ");
+
+        this.myText = this._myOriginalText.slice(0);
+        this.myText = countString.concat(this.myText);
+        this.myLines = this.myText.split("\n");
     }
 };
